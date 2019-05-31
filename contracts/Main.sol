6f054e0c4d7704f5dc704c63ec17c7b4e3bee6d4
wickstjo <jfwick@gmail.com>
2019-05-31 11:31:43 +0300

    Started mapping out smart contract structure

--- contracts/Main.sol
+++ contracts/Main.sol
@@ -1,22 +1,97 @@
 pragma solidity ^0.5.0;
-pragma experimental ABIEncoderV2;
 
 contract Main {
 
-    string[] public people;
-
-    // ADD PERSON
-    function add(string memory person) public {
-        people.push(person);
+    // USER STRUCT
+    struct User {
+        address owner;
+        string name;
+        Device[] devices;
     }
 
-    // FETCH PERSON
-    function person(uint id) public view returns(string memory) {
-        return people[id];
+    // DEVICE STRUCT
+    struct Device {
+        address id;
+        string name;
     }
 
-    // FETCH EVERYONE
-    function everyone() public view returns(string[] memory) {
-        return people;
+    // ARRAY OF USERS
+    User[] users;
+
+    // ADD USER
+    function add_user(address _owner, string memory _name) public {
+
+        // MAKE SURE THAT USER DOESNT EXIST
+        if (!user_exists(_owner)) {
+
+            // CREATE TEMP DEVICE ARRAY
+            Device[] memory temp;
+
+            // PUSH NEW USER OBJECT TO ARRAY
+            users.push(User({
+                owner: _owner,
+                name: _name,
+                devices: temp
+            }));
+        }
+    }
+
+    // CHECK IF USER EXISTS
+    function user_exists(address user) internal view returns(bool) {
+
+        // DEFAULT TO FALSE
+        bool response = false;
+
+        // LOOP THROUGH USERS
+        for(uint i = 0; i < users.length; i++) {
+
+            // IF ITS FOUND -- CHANGE RESPONSE TO TRUE & STOP LOOPING
+            if (users[i].owner == user) {
+                response = true;
+                break;
+            }
+        }
+
+        // FINALLY RETURN
+        return response;
+    }
+
+    // CHECK IF USER EXISTS
+    function device_exists(address owner, address device) internal view returns(bool) {
+
+        // DEFAULT TO FALSE
+        bool response = false;
+
+        // MAKE SURE OWNER EXISTS
+        if (user_exists(owner)) {
+
+            // FETCH USER
+            User memory user = find_user(owner);
+
+            // LOOP THROUGH DEVICES
+            for(uint i = 0; i < user.devices.length; i++) {
+
+                // IF ITS FOUND -- CHANGE RESPONSE TO TRUE & STOP LOOPING
+                if (user.devices[i].id == device) {
+                    response = true;
+                    break;
+                }
+            }
+        }
+
+        // FINALLY RETURN
+        return response;
+    }
+
+    function find_user(address owner) internal view returns(User memory) {
+
+        // LOOP THROUGH
+        for(uint i = 0; i < users.length; i++) {
+
+            // IF ITS FOUND -- CHANGE RESPONSE TO TRUE & STOP LOOPING
+            if (users[i].owner == owner) {
+                return users[i];
+            }
+        }
     }
 }
